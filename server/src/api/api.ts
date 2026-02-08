5a00bad9c587fb60fd3c5910736d0c9156a94555
kanav789 <kanavrana91@gmail.com>
2026-02-08 10:16:37 +0530

    did the chnage

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/status", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ status: "API is running" });
 });
 
